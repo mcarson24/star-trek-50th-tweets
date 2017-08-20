7d65b82fa01bfa377a61cab55edf010c5050f3aa
Matthew Carson <matthew24carson@gmail.com>
2017-08-20 17:55:51 -0400

    Add function to convert twitter handles to links. Allow for _ in links.

--- app/Tweet.php
+++ app/Tweet.php
@@ -24,11 +24,16 @@ class Tweet
 
 	public function text()
 	{
-		return $this->convert_links_to_anchor_tags($this->text);
+		return $this->convert_twitter_handles_to_link($this->convert_links_to_anchor_tags($this->text));
 	}
 
 	private function convert_links_to_anchor_tags($text)
 	{
-		return preg_replace('/(https:\/\/[a-z.\/0-9]*)/i', '<a href="$1">$1</a>', $text);
+		return preg_replace('/(https:\/\/[a-z.\/0-9_]*)/i', '<a href="$1">$1</a>', $text);
+	}
+
+	private function convert_twitter_handles_to_link($text)
+	{
+		return preg_replace('/@(\w+)/', '<a href="https://twitter.com/$1">@$1</a>', $text);
 	}
 }
