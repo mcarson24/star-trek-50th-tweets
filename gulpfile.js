da96df9d6ebee4e1a924124b1073a6813dca0e42
Matthew Carson <matthew24carson@gmail.com>
2017-08-21 02:15:54 -0400

    Begin styling.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+  var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return sass('scss/style.scss', {
+      sourcemap: true,
+      style: 'compressed'
+    })
+    .on('error', function (err) {
+        console.error('Error!', err.message);
+    })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('views/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['scss/**/*'], ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
