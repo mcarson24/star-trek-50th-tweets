4f352e1ec1a40eebe634705a6e276982542af609
Matthew Carson <matthew24carson@gmail.com>
2017-10-04 17:33:18 -0400

    I keep coming back to this

--- app/TweetLoader.php
+++ app/TweetLoader.php
@@ -7,30 +7,30 @@ use Illuminate\Support\Collection;
 
 class TweetLoader
 {
-	protected $tweets;
+    protected $tweets;
 
-	/**
-	 * Loads the csv file and converts all items to Tweet Objects.
-	 * 
-	 * @param  $file 
-	 * @return Illuminate\Support\Collection
-	 */
-	public function load($file)
-	{
-		$this->tweets = collect(fgetcsv(fopen($file, 'r'), 1050000, '^'));
+    /**
+     * Loads the csv file and converts all items to Tweet Objects.
+     *
+     * @param  $file
+     * @return Illuminate\Support\Collection
+     */
+    public function load($file)
+    {
+        $this->tweets = collect(fgetcsv(fopen($file, 'r'), 1050000, '^'));
 
-		return $this->toTweets();
-	}
+        return $this->toTweets();
+    }
 
-	/**
-	 * Convert each tweet into a Tweet Object.
-	 * 
-	 * @return Illuminate\Support\Collection
-	 */
-	private function toTweets()
-	{
-		return $this->tweets->map(function($tweet) {
-			return new Tweet($tweet);
-		});
-	}
+    /**
+     * Convert each tweet into a Tweet Object.
+     *
+     * @return Illuminate\Support\Collection
+     */
+    private function toTweets()
+    {
+        return $this->tweets->map(function ($tweet) {
+            return new Tweet($tweet);
+        });
+    }
 }
