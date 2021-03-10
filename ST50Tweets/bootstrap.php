f080030570f5e664f918f019641bd2f52f7055f8
Matthew Carson <matthew.carson@pm.me>
2021-03-09 20:40:48 -0500

    Majoir change

--- ST50Tweets/bootstrap.php
+++ ST50Tweets/bootstrap.php
@@ -7,4 +7,4 @@ $dotenv = new Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
 
-Container::bind('database', require __DIR__ . '/database/config.php');
+Container::bind('database', require __DIR__ . '/Database/config.php');
